92eebc570a7417c3eb766b80da11b79338df1686
Santosh <santoshsantoshsthastha@gmail.com>
2024-12-11 08:50:15 +0545

    learned about the templates and about the ways to configure the views.py aslo the urls.py

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Santosh',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'December 11, 2024'
+    },
+        {
+        'author': 'Saurab',
+        'title': 'Blog Post 2',
+        'content': 'Sencond post in the blog content',
+        'date_posted': 'December 11, 2024'
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
